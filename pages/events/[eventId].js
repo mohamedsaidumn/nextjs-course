68346e1d96750df1b1b7d6e32f937cbf72e03e90
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-10-08 18:18:53 -0500

    Adding the Event Detail  Page Dynamic Route

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,34 @@
+import { Fragment } from "react";
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { useRouter } from "next/router";
+import { getEventById } from "@/data/dummy-data";
+
 function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  console.log(eventId);
+  const event = getEventById(eventId);
+  console.log(event);
+
+  if (!event) {
+    return <p> No event found</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page </h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.imageAlt}
+      />
+      <EventContent>
+        <p>{eventId.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
