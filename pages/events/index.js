40efa380586a1807e38dfdc75cc178685b0d3b7d
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-10-09 22:19:59 -0500

    added jsx for the search component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import { getAllEvents } from "@/data/dummy-data";
 import { EventList } from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
 function AllEventsPage() {
   const events = getAllEvents();
 
   return (
     <div>
+      <EventsSearch />
       <EventList items={events} />
     </div>
   );
