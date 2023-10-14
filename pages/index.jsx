fa97077e3b2e9b01b641b8eb9f4385d7960ef70a
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-10-14 18:00:03 -0500

    a closer look at get static probs and confguration options using redirect and notFound

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,14 +16,30 @@ function HomePage(props) {
 }
 
 export async function getStaticProps(context) {
+  console.log("(Re-)Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  // //If the there is no data redirect to a different page.
+  // if (!data) {
+  //   return {
+  //     redirect: {
+  //       destination: "/no-data",
+  //     },
+  //   };
+  // }
+
+  // //If the data length is 0 return not found page
+  // if (data.products.length === 0) {
+  //   return { notFound: true };
+  // }
 
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
