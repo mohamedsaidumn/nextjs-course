91090ca803c69fd8f702e27ead01de27c653d5db
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-10-17 09:02:36 -0500

    Combining Pre-Fetching With Client-Side Fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
+
 function LastSalesPage(props) {
-  const [sales, setSales] = useState();
-  //   const [isLoading, setIsLoading] = useState(false);
+  const [sales, setSales] = useState(props.sales);
+  // const [isLoading, setIsLoading] = useState(false);
+
   const { data, error } = useSWR(
     "https://nextjs-course-8b7d3-default-rtdb.firebaseio.com/sales.json",
     (url) => fetch(url).then((res) => res.json())
@@ -24,39 +26,31 @@ function LastSalesPage(props) {
     }
   }, [data]);
 
-  //   useEffect(() => {
-  //     setIsLoading(true);
-  //     fetch("https://nextjs-course-8b7d3-default-rtdb.firebaseio.com/sales.json")
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         const transformedSales = [];
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://nextjs-course-8b7d3-default-rtdb.firebaseio.com/sales.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
 
-  //         for (const key in data) {
-  //           transformedSales.push({
-  //             id: key,
-  //             username: data[key].username,
-  //             volume: data[key].volume,
-  //           });
-  //         }
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
 
-  //         setSales(transformedSales);
-  //         setIsLoading(false);
-  //       });
-  //   }, []);
-
-  //   if (isLoading) {
-  //     return <p>Loading...</p>;
-  //   }
-
-  //   if (!sales) {
-  //     return <p>No Data Yet</p>;
-  //   }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
   if (error) {
     return <p>Failed to load.</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -69,6 +63,25 @@ function LastSalesPage(props) {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-8b7d3-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales } };
 }
 
 export default LastSalesPage;
