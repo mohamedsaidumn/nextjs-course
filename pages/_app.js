f247ca78dc4ac22ac40a3306d189872bef635918
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-11-14 09:13:09 -0600

    Merging head Content

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "../styles/globals.css";
 import Layout from "@/components/layout/layout";
+import Head from "next/head";
 
 function MyApp(props) {
   const { Component, pageProps } = props;
   return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name="description" content="NextJS Events" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />;
     </Layout>
   );
