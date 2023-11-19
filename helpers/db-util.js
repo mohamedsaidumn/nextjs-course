25065a0e91b506e573c7460b512edb2c90b73195
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-11-19 12:11:05 -0600

    Improvement: Getting Comments For A Specific Event

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -16,9 +16,13 @@ export async function insertDocument(client, collection, document) {
   return result;
 }
 
-export async function getAllDocuments(client, collection, sort) {
+export async function getAllDocuments(client, collection, sort, filter = {}) {
   const db = client.db();
-  const documents = await db.collection(collection).find().sort(sort).toArray();
+  let documents = await db
+    .collection(collection)
+    .find(filter)
+    .sort(sort)
+    .toArray();
 
   return documents;
 }
