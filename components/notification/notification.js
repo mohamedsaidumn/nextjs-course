0ef95d4c187e7496e09d878cb421bbc51339f3c7
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-11-19 21:30:04 -0600

    Triggering & Showing Notifications

--- components/notification/notification.js
+++ components/notification/notification.js
@@ -24,7 +24,7 @@ function Notification(props) {
   const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    <div className={activeClasses}>
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
