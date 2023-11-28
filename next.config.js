f2dc28c987aeb4da5494b2b74d631187957dae0d
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-11-28 12:16:38 -0600

    added signup api route

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    mongodb_username: "mohamedsaidumn",
+    mongodb_password: "3kRu3Ni2fucYhLGp",
+    mongodb_clustername: "cluster04211",
+    mongodb_database: "auth-demo",
+  },
+};
